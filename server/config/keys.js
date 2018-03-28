a55b4c2fe3dd480ac8cda1ce857298433586d8ff
Brandon P <brandonpegula@gmail.com>
2018-03-28 17:14:21 -0400

    Servers are synced with Yarn

--- server/config/keys.js
+++ server/config/keys.js
@@ -4,5 +4,5 @@ if(process.env.NODE_ENV === "production") {
 module.exports = require('./prod');
 } else {
 //return development set of keys
-module.exports = require('./div');
+module.exports = require('./dev');
 }
