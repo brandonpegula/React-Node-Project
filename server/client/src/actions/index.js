14a3c9486319a565b9b105f09767a8c4643c52af
Brandon P <brandonpegula@gmail.com>
2018-03-29 17:12:57 -0400

    Full Oauth flow in Header

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,7 +3,6 @@ import { FETCH_USER } from "./types";
 
 
 export const fetchUser = () => async dispatch => {
-    axios
-        .get("/api/current_user")
-        .then(res => dispatch({ type: FETCH_USER, payload: res }));
+    const res = await axios.get("/api/current_user");
+    dispatch({ type: FETCH_USER, payload: res.data });
 };
